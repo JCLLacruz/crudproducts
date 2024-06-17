5ae5a2802174f1076c926a97090af9650453ed53
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-17 12:56:30 +0200

    create

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,32 +2,73 @@
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
-	computed: {
-		...mapGetters(['allProducts']),
-	},
-	methods: {
-		...mapActions(['fetchProducts']),
-	},
-	created() {
-		this.fetchProducts();
-	},
+    data() {
+        return {
+            productName: '',
+            description: '',
+            price: '',
+            imagePath: '',
+        };
+    },
+    computed: {
+        ...mapGetters(['allProducts']),
+    },
+    methods: {
+        ...mapActions(['fetchProducts', 'createProduct']),
+        async addProduct() {
+            const product = {
+                productName: this.productName,
+                description: this.description,
+                price: this.price,
+                imagePath: this.imagePath,
+            };
+            await this.createProduct(product);
+            this.productName = '';
+            this.description = '';
+            this.price = '';
+            this.imagePath = '';
+        },
+    },
+    created() {
+        this.fetchProducts();
+    },
 };
 </script>
 
+
 <template>
-	<div>
-		<h1>Mis productos</h1>
-		<button>Create Product</button>
-		<div v-for="product in allProducts" :key="product._id">
-			{{ product.productName }}
-			<button>Update Product</button>
-			<button>Delete</button>
-		</div>
-	</div>
+  <div>
+      <h1>Mis productos</h1>
+      <form @submit.prevent="addProduct">
+          <div class="">
+              <label for="productName">Nombre del producto:</label>
+              <input v-model="productName" type="text" id="productName" required />
+              
+              <label for="description">Descripción:</label>
+              <input v-model="description" type="text" id="description" required />
+              
+              <label for="price">Precio:</label>
+              <input v-model="price" type="number" id="price" required />
+              
+              <label for="imagePath">URL imagen:</label>
+              <input v-model="imagePath" type="text" id="imagePath" required />
+          </div>
+          <button type="submit">Crear producto</button>
+      </form>
+      <div v-for="product in allProducts" :key="product._id">
+          {{ product.productName }}
+          <button>Update Product</button>
+          <button>Delete</button>
+      </div>
+  </div>
 </template>
+
 
 <style scoped>
 h1 {
 	color: red;
+}
+.hidden {
+  display: none;
 }
 </style>
